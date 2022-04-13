f1d11eaf3b068f0f0337ae0c55b9f8ff068856b1
giampa95 <giampa95@hotmail.com>
2022-04-13 18:48:43 -0300

    updating template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,23 @@
 import os
 import subprocess
 
+def env_exist(env_name):
+    """Check if an env_name is already used/created"""
+    enviroments  = os.popen('conda env list').readlines()
+
+    for line in enviroments:
+        if f'envs/{env_name}' in line:
+            return True
+    
+    return False
+
 
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 project_packages = "{{ cookiecutter.project_packages }}"
+
+
+
 
 
 print(f"{MESSAGE_COLOR}Almost done!")
@@ -13,5 +26,17 @@ print(f"Initializing a git repository...{RESET_ALL}")
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+print(f"{MESSAGE_COLOR}Virtual environment:{RESET_ALL}")
+
+
+if env_exist(project_packages):
+    print(f"{MESSAGE_COLOR}Run conda activate {project_packages} {RESET_ALL}")
+
+else:
+    
+    env_path = project_packages +'_'+'environment.yaml'
+    print(f"{MESSAGE_COLOR}Run conda env create -f {env_path} {RESET_ALL}")
+
 
 print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
